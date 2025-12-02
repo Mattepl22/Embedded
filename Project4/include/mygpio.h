3e7668d714b24170ee448501bbb84cb04bb20622
Mattepl22 <matteo.poli.1997@gmail.com>
2025-12-02 21:06:31 +0100

    Iniziato con stampa seriale

---
+++ Project4/include/mygpio.h
@@ -0,0 +1,53 @@
+#ifndef MYGPIO_H
+#define MYGPIO_H
+
+#include <Arduino.h>
+
+typedef struct {
+    uint32_t bt_select;     //Offset 0x00
+    uint32_t out;           //Offset 0x04
+    uint32_t out_w1ts;      //Offset 0x08
+    uint32_t out_w1tc;      //Offset 0x0C
+
+    uint32_t out1;          //Offset 0x10
+    uint32_t out1_w1ts;     //Offset 0x14
+    uint32_t out1_w1tc;     //Offset 0x18
+
+    uint32_t unused_1;      //Offset 0x1C
+
+    uint32_t enable;        //Offset 0x20
+    uint32_t enable_w1ts;   //Offset 0x24
+    uint32_t enable_w1tc;   //Offset 0x28
+
+    uint32_t enable1;       //Offset 0x2C
+    uint32_t enable1_w1ts;  //Offset 0x30
+    uint32_t enable1_w1tc;  //Offset 0x34
+
+    uint32_t strap;         //Offset 0x38
+
+    uint32_t in;            //Offset 0x3C
+    uint32_t in1;           //Offset 0x40    
+} GPIO_Regs;
+
+typedef struct {
+    uint32_t fifo;      //Offset 0x00
+
+    uint32_t int_raw;   //Offset 0x04
+    uint32_t int_st;    //Offset 0x08
+    uint32_t int_ena;   //Offset 0x0C
+    uint32_t int_clr;   //Offset 0x10
+
+    uint32_t clkdiv;    //Offset 0x14
+    uint32_t autobaud;  //Offset 0x18
+
+    uint32_t status;    //Offset 0x1C
+
+    uint32_t conf0;     //Offset 0x20
+    uint32_t conf1;     //Offset 0x24
+
+    uint32_t lowpulse;  //Offset 0x28
+    uint32_t highpulse; //Offset 0x2C
+    uint32_t rxd_cnt;   //Offset 0x30
+} UART_Regs;
+
+#endif
---
+++ Project4/src/main.cpp
@@ -0,0 +1,50 @@
+#include "mygpio.h"
+
+//Definisco INPUT/OUTPUT
+#define PIN_LED 2
+#define PIN_BUTTON 4
+
+#define DELAY_TIME 200 //Tempo di delay tra una pressione e la successiva
+#define TIMER_NUMBER 0 //Periferica del timer da usare
+#define PRESCALER 80 //Prescaler del timer per avere 1MHz = 1 tick = 1microsec 
+#define TIME_TRIG 500000 //500ms = 500.000 microsec
+
+hw_timer_t *timer = NULL;
+
+//Inizializzo la struttura e dico che parte dall'indirizzo scritto
+volatile GPIO_Regs *myGPIO = (volatile GPIO_Regs *)0x3FF44000;
+volatile UART_Regs *myUART = (volatile UART_Regs *)0x3FF40000;
+
+void IRAM_ATTR onTimer();
+void myPutChar(char c);
+
+void setup() {
+  Serial.begin(115200);
+
+  //Imposto GPIO2 come OUTPUT
+  myGPIO->enable_w1ts = (1 << PIN_LED);
+
+  //Imposto GPIO4 come INPUT
+  myGPIO->enable_w1tc = (1 << PIN_BUTTON);
+
+  //Inizializzazione del timer
+  timer = timerBegin(TIMER_NUMBER, PRESCALER, true);
+  //Attacco l'ISR al timer
+  timerAttachInterrupt(timer, &onTimer, true); 
+  //Setto l'evento ogni 500ms e ricarico
+  timerAlarmWrite(timer, TIME_TRIG, true);
+  //Abilito l'allarme
+  timerAlarmEnable(timer); 
+}
+
+void loop() {
+
+}
+
+void myPutChar(char c) {
+  myUART->fifo = c;
+}
+
+void IRAM_ATTR onTimer() {
+  myPutChar('A');
+}
